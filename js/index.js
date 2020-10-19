522dcbe952ec66f152c36e20e86bf7426e176511
Семён Денисов <69531676+Samaelish@users.noreply.github.com>
2020-10-19 19:38:07 +0300

    Add files via upload
    Portfolio Website

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Variables
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+// Actions
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    });
+});
